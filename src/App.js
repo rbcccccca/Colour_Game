2c5b5031c345645ff4f4f453f19d39b94369ea61
rbcccccca <roujiaf@student.unimelb.edu.au>
2024-09-30 18:50:13 +1000

    checked

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       {currentGame === 'home' && (
-        <Home 
+        <Home
           onStartFallingGame={startFallingGame}
           onStartMatcherGame={startMatcherGame}
         />
--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home({ onStartFallingGame, onStartMatcherGame }) {
   return (
-    <div className="home">
-      <h1>Color Game Series</h1>
+    <div className="Home">
+      <h1>Welcome to Color Games</h1>
       <button onClick={onStartFallingGame}>Play Falling Color Game</button>
       <button onClick={onStartMatcherGame}>Play Color Matcher Game</button>
     </div>
