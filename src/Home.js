86aa14c6f04ae2dbfc01bfb4ff6d39a02c83a8a2
rbcccccca <roujiaf@student.unimelb.edu.au>
2024-09-30 18:54:08 +1000

    home page updated

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './Home.css'; // Make sure to create this CSS file
 
 function Home({ onStartFallingGame, onStartMatcherGame }) {
   return (
-    <div className="Home">
-      <h1>Welcome to Color Games</h1>
-      <button onClick={onStartFallingGame}>Play Falling Color Game</button>
-      <button onClick={onStartMatcherGame}>Play Color Matcher Game</button>
+    <div className="home-container">
+      <div className="floating-shapes">
+        <div className="shape circle"></div>
+        <div className="shape triangle"></div>
+        <div className="shape square"></div>
+        <div className="shape pentagon"></div>
+      </div>
+      <div className="content">
+        <h1 className="title">Welcome to Color Games!</h1>
+        <div className="button-container">
+          <button className="game-button" onClick={onStartFallingGame}>
+            Play Falling Color Game
+          </button>
+          <button className="game-button" onClick={onStartMatcherGame}>
+            Play Color Matcher Game
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
